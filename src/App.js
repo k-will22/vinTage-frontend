6a14ac5a41fe3581afa63f71fbcc9aa6d1643a27
k-will22 <thestreetkw@hotmail.com>
2021-02-25 22:07:48 -0500

    coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">vinTage</h1>
+      <p>One stop shop for all your vintage t-shirt needs!</p>
+      <p><strong>Coming Soon!</strong></p>
+      <img 
+      src="https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:1400/output=quality:70/compress/https://cdn.fs.grailed.com/api/file/AKXqF3qXSX2Fa1ih5O7X" 
+      alt="Green Day tour shirt"></img>
+      <img 
+      src="https://cdn.shopify.com/s/files/1/2313/4943/products/IMG_6929_clean_2048x.jpg?v=1569882716" 
+      alt="Bush tour shirt"></img>
+      <img 
+      src="https://img.secure.cdn2.wmgecom.com/media/catalog/product/cache/1835/image/800x/9df78eab33525d08d6e5fb8d27136e95/t/s/tsp-infititesadnesstour.jpg" 
+      alt="Smashing Pumpkins tour shirt"></img>
+      <img src="https://thumbs.worthpoint.com/zoom/images3/1/0317/26/weezer-vintage-shirt_1_785b801e79f2f7a35fb8ae559c38933a.jpg"
+      alt="Weezer shirt"></img>
     </div>
   );
 }
